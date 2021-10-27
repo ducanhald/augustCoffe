af48edd8ad80e3d51fd01e9508b823933d27caf2
ducanhald <ducanhald@gmail.com>
2021-10-28 00:04:24 +0700

    commit json file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import Validator from "./validate";
+import Validator from "./Validator.js";
 const $ = document.querySelector.bind(document);
 const $S = document.querySelector.bind(document);
 const modalSignin = $(".modal-signin");
@@ -14,8 +14,6 @@ const iconmenus = $(".s-icon-menu");
 const menuhide = $(".menu-hide");
 const menuhidecontainer = $(".menu-hide-container");
 const lycf = $(".img-lycf");
-
-isSign = false;
 
 function showModal(modal) {
   modal.classList.add("open");
@@ -76,4 +74,120 @@ menuhide.onclick = function () {
 //     changeImg()
 // }
 // Validate
-Validator;
+
+Validator({
+  form: "#form-signin",
+  formGroup: ".form-group",
+  errorSelector: ".form-message",
+  rules: [
+    Validator.isRequired("#fullname"),
+    Validator.isRequired("#email", "* Please enter a email"),
+    Validator.isEmail("#email", " * Invalid email"),
+    Validator.isRequired("#password", "* Please enter a password"),
+    // Validator.minLenght("#password", 2),
+    Validator.isRequired(
+      "#password_confirmation",
+      "Vui lòng xác nhận mật khẩu"
+    ),
+    Validator.isConfirmed(
+      "#password_confirmation",
+      function () {
+        return document.querySelector("#form-1 #password").value;
+      },
+      "Mật khẩu nhập lại không chính xác"
+    ),
+  ],
+  onSubmit: function (data) {
+    console.log(data);
+  },
+});
+//
+// get productItem dom
+const productList = $(".home-product-list");
+console.log(productList);
+
+var productItems = [
+  {
+    name: "Máy pha cà phê 1",
+    priceold: 1200000,
+    pricecurrent: 800000,
+    brand: " Samsung",
+    originname: "Hàn quốc",
+  },
+  {
+    name: "Máy pha cà phê 2",
+    priceold: 1200000,
+    pricecurrent: 800000,
+    brand: " Samsung",
+    originname: "Hàn quốc",
+  },
+  {
+    name: "Máy pha cà phê 5",
+    priceold: 1200000,
+    pricecurrent: 800000,
+    brand: " Samsung",
+    originname: "Hàn quốc",
+  },
+  {
+    name: "Máy pha cà phê 6",
+    priceold: 1200000,
+    pricecurrent: 800000,
+    brand: " Samsung",
+    originname: "Hàn quốc",
+  },
+  {
+    name: "Máy pha cà phê 7",
+    priceold: 1200000,
+    pricecurrent: 800000,
+    brand: " Samsung",
+    originname: "Hàn quốc",
+  },
+];
+var html = productItems.map((item) => {
+  return `<div class="col l-2-4 m-4 c-12 color home-product-item ">
+  <a class="home-product-item__link">
+    <div
+      class="home-product-item__img"
+      style="background-image: url(./assets/imgs/MayPhacf.png)"
+    ></div>
+    <div class="home-product-item__content">
+      <p class="home-product-item__name">
+        ${item.name}
+      </p>
+      <div class="home-product-item__price">
+        <span class="home-product-item__price-old">1.200.000</span>
+        <span class="home-product-item__price-current"
+          >888.888</span
+        >
+      </div>
+      <div class="home-product-item__action">
+        <span class="home-product-item__like">
+          <i
+            class="home-product-item__like-icon-empty far fa-heart"
+          ></i>
+          <i
+            class="home-product-item__like-icon-fill fas fa-heart"
+          ></i>
+        </span>
+        <div class="home-product-item__rating">
+          <i class="home-product-item__star--gold fas fa-star"></i>
+          <i class="home-product-item__star--gold fas fa-star"></i>
+          <i class="home-product-item__star--gold fas fa-star"></i>
+          <i class="home-product-item__star--gold fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+      </div>
+      <div class="home-product-item__origin">
+        <span class="home-product-item__brand">SamSung</span>
+        <span class="home-product-item__origin-name">Hàn Quóc</span>
+      </div>
+      
+      <div class="home-product-item__sale-off">
+        <span class="home-product-item__sale-off-percent">10%</span>
+        <span class="home-product-item__sale-off-label">GIẢM</span>
+      </div>
+    </div>
+  </a>
+</div>`;
+});
+productList.innerHTML = html.join("");
